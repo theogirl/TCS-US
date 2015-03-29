f3fe4a83ee07048b28729faa9c7dd17723d07a05
theogirl <theogirl@outlook.com>
2015-03-29 18:44:11 -0400

    Final files for ininitial TCS-US launch

---
+++ sitemap.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="The Cup Store is your premier source for custom printed disposable paper and plastic cups, glassware, containers, coffee sleeves and much more. Grow your brand and your business one cup at a time - print your logo or other artwork on a cup!">
+	<meta name="keywords" content="paper cups, plastic cups, glassware, coffee sleeves, gelato cups, ice cream cups, compostable cups, biodegradeable cups, eco cups, printed paper cups, printed plastic cups, custom printed cups, imprinted paper cups, imprinted plastic cups, promotional cups, logo on cups, custom cups, customized cups">
+    
+    <title>404 Error | The Cup Store</title>
+
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/tcs-style.css">
+	<link rel="stylesheet" href="css/font-awesome.css">
+
+	<link rel="icon" type="image/png" href="images/favicon-32x32.png" sizes="32x32">
+	<link rel="icon" type="image/png" href="images/android-chrome-192x192.png" sizes="192x192">
+	<link rel="icon" type="image/png" href="images/favicon-96x96.png" sizes="96x96">
+	<link rel="icon" type="image/png" href="images/favicon-16x16.png" sizes="16x16">
+
+	<!--TypeKit fonts-->
+	<script src="//use.typekit.net/eco7azw.js"></script>
+	<script>try{Typekit.load();}catch(e){}</script>
+	
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<script scr="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	<![endif]-->	
+
+<!--Google Analytics tracking CAN-->
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-19378007-2']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+</head>
+
+<body>
+
+<!--HEADER SECTION-->
+<?php include 'includes/header.php'; ?>
+
+<!--MAIN CONTENT-->
+
+<!--PAGE TITLE-->
+<section class="tcs-pagetitle">
+	<div class="container-fluid">
+		<h1>Site Map</h1>
+	</div>
+</section>
+
+<!--SITEMAP-->
+
+<div class="container">
+<h1>HTML Site Map</h1>
+<h3><a href="http://www.thecupstore.com">Homepage</a><br /> Last updated: 2015, March 29</h3>
+
+<div class="row">
+
+<div class="col-xs-12 col-sm-4">
+<ul class="sitemap">
+<li><a href="http://www.thecupstore.com/index.php" title="The Cup Store">The Cup Store</a></li>
+<li><a href="http://www.thecupstore.com/products.php" title="Products | The Cup Store">Products | The Cup Store</a></li>
+<li><a href="http://www.thecupstore.com/prod-paper-cups.php" title="Paper Cups | The Cup Store">Paper Cups | The Cup Store</a></li>
+<li><a href="http://www.thecupstore.com/prod-eco-paper-cups.php" title="Eco-Paper Cups | The Cup Store">Eco-Paper Cups | The Cup Store</a></li>
+<li><a href="http://www.thecupstore.com/prod-double-paper-cups.php" title="Double-Walled Paper Cups | The Cup Store">Double-Walled Paper Cups | The Cup Store</a></li>
+<li><a href="http://www.thecupstore.com/prod-plastic-cups.php" title="Plastic Cups | The Cup Store">Plastic Cups | The Cup Store</a></li>
+<li><a href="http://www.thecupstore.com/prod-eco-plastic-cups.php" title="Plastic Cups | The Cup Store">Plastic Cups | The Cup Store</a></li>
+<li><a href="http://www.thecupstore.com/prod-frosted-plastic-cups.php" title="Plastic Cups | The Cup Store">Plastic Cups | The Cup Store</a></li>
+</ul></div>
+
+<div class="col-xs-12 col-sm-4">
+<ul class="sitemap">
+<li><a href="http://www.thecupstore.com/prod-glassware.php" title="Glassware | The Cup Store">Glassware | The Cup Store</a></li>
+<li><a href="http://www.thecupstore.com/prod-coffee-sleeves.php" title="Coffee Sleeves | The Cup Store">Coffee Sleeves | The Cup Store</a></li>
+<li><a href="http://www.thecupstore.com/prod-gelato-cups.php" title="Gelato Cups | The Cup Store">Gelato Cups | The Cup Store</a></li>
+<li><a href="http://www.thecupstore.com/order-info.php" title="Order Info | The Cup Store">Order Info | The Cup Store</a></li>
+<li><a href="http://www.thecupstore.com/about-us.php" title="About Us | The Cup Store">About Us | The Cup Store</a></li>
+<li><a href="http://www.thecupstore.com/contact-us.php" title="Contact Us | The Cup Store">Contact Us | The Cup Store</a></li>
+<li><a href="http://www.thecupstore.com/404-error.php" title="404 Error | The Cup Store">404 Error | The Cup Store</a></li>
+</ul></div>
+
+</div>
+
+<p>Form: 1 page</p>
+<p><a href="http://www.thecupstore.com/form/quote-request.php" title="Quote Request | The Cup Store">Quote Request | The Cup Store</a></p>
+
+
+<!--
+Please note:
+You are not allowed to remove the copyright notice below.
+Thank you!
+www.xml-sitemaps.com
+-->
+<p>Page generated by XML-Sitemaps.com - <a target="_blank" href="https://www.xml-sitemaps.com">Google XML sitemap and html sitemaps generator</a>
+|
+Copyright &copy; 2005-2014</p>
+
+</div>
+</div>
+
+</div>
+
+<!--FOOTER SECTION-->
+
+<?php include 'includes/footer.php'; ?>
+
+
+    <!-- Latest compiled and minified JavaScript 
+    ================================================== -->
+	
+	<script type="text/javascript" src="js/jquery-1.11.2.js"></script>
+	<script type="text/javascript" src="bootstrap.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+</body>
+
+</html>
